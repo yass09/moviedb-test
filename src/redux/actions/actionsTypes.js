6a2794cdf9aa97d2667dca20939fab0d5f973cfa
Yacine Chibane-Devereux <y.chibane@me.com>
2019-11-10 20:51:46 +0100

    edited action types

--- src/redux/actions/actionsTypes.js
+++ src/redux/actions/actionsTypes.js
@@ -1,5 +1,10 @@
+// Load Movie List actions
 export const LOAD_MOVIES = 'LOAD_MOVIES';
 export const LOAD_MOVIES_SUCCESS = 'LOAD_MOVIES_SUCCESS';
 export const LOAD_MOVIES_ERROR = 'LOAD_MOVIES_ERROR';
+
+// Sort by release date
 export const SORT_MOVIES_RELEASEDATE = 'SORT_MOVIES_RELEASEDATE';
+
+// Select Movie for details request
 export const SELECT_MOVIE = 'SELECT_MOVIE';
