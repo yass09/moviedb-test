2c87233b0bacc87c20ba57a609d7c63f249df77c
Yacine Chibane-Devereux <y.chibane@me.com>
2019-11-11 17:32:04 +0100

    edited fetch helper function

--- src/helpers.js
+++ src/helpers.js
@@ -11,8 +11,12 @@ import {
 
 const movieListUrl = `${BASE_URL}${MOVIES_URL}${API_KEY}${LANGUAGE_PARAM}${SORT_BY_POPULARITY}${OTHER_PARAMS}${PAGE_PARAM}`;
 
-export const getMovies = (pageNumber = 1) => {
-	fetch(`${movieListUrl}${pageNumber}`)
-		.then(response => response.json())
-		.then(json => console.log(json));
+export const getMoviesHelper = async (pageNumber = 1) => {
+	try {
+		const response = await fetch(`${movieListUrl}${pageNumber}`);
+		const data = await response.json();
+		return data;
+	} catch (e) {
+		throw Error(e);
+	}
 };
