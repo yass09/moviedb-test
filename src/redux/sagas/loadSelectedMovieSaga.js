2386fc3042abc45c6c94bd299b213f2ed5fec323
Yacine Chibane-Devereux <y.chibane@me.com>
2019-11-13 22:42:36 +0100

    added movie details saga

---
+++ src/redux/sagas/loadSelectedMovieSaga.js
@@ -0,0 +1,23 @@
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable no-console */
+import { call, put } from 'redux-saga/effects';
+
+import { getData } from '../../helpers';
+import { getMovieDetailsUrl } from '../../constants';
+
+import {
+	loadMovieDetailsSuccess,
+	loadMovieDetailsError,
+} from '../actions/movieDetailsActions';
+
+export function* loadSelectedMovieWorker(action) {
+	const url = getMovieDetailsUrl(action.id);
+	try {
+		const data = yield call(() => getData(url));
+		yield put(loadMovieDetailsSuccess(data));
+		console.log(data);
+	} catch (e) {
+		yield put(loadMovieDetailsError(e));
+		console.log('error', e);
+	}
+}
