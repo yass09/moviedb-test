e975162771358811abe392be227be611cef50e9a
Yacine Chibane-Devereux <y.chibane@me.com>
2019-11-13 22:45:55 +0100

    added data fetching to details view

--- src/components/containers/MovieDetailsView.js
+++ src/components/containers/MovieDetailsView.js
@@ -1,8 +1,38 @@
-import React from 'react';
-import MovieListView from './MovieListView';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const MovieDetailsView = () => {
+import { loadMovieDetails } from '../../redux/actions/movieDetailsActions';
+import { rootSaga } from '../../redux/sagas';
+
+const mapStateToProps = state => ({
+	movie: state.selectedMovie.movieDetails,
+	loading: state.loading,
+	error: state.error,
+});
+
+const mapDispatchToProps = dispatch => ({
+	getMovieDetails: id => dispatch(loadMovieDetails(id)),
+	rootSaga,
+});
+
+const MovieDetailsView = props => {
+	const { getMovieDetails, match: { params: { id } = {} } = {} } = props;
+	useEffect(() => {
+		const getMovieDetailsOnLoad = () => getMovieDetails(id);
+		getMovieDetailsOnLoad();
+	}, [getMovieDetails, id]);
+
 	return <div> Movie Details</div>;
 };
 
-export default MovieDetailsView;
+MovieDetailsView.propTypes = {
+	getMovieDetails: PropTypes.func.isRequired,
+	match: PropTypes.shape({
+		params: PropTypes.shape({
+			id: PropTypes.string,
+		}),
+	}).isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MovieDetailsView);
