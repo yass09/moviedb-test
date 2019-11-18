582740ec7fb416e311da051f7baadb0c8052c128
Yacine Chibane-Devereux <y.chibane@me.com>
2019-11-18 01:58:14 +0100

    fixed reducer

--- src/redux/reducers/setOrderByReducer.js
+++ src/redux/reducers/setOrderByReducer.js
@@ -3,10 +3,7 @@ import { SET_ORDER_BY } from '../actions/actionsTypes';
 const setOrderByReducer = (state = '', action) => {
 	switch (action.type) {
 		case SET_ORDER_BY:
-			return {
-				...state,
-				orderBy: action.order,
-			};
+			return action.order;
 		default:
 			return state;
 	}
