2d4cb0e8b8b73ba979f99fd22e8b26228bdf074c
Yacine Chibane-Devereux <y.chibane@me.com>
2019-11-18 01:58:45 +0100

    added switch orderby in movieList

--- src/components/containers/MovieListView.js
+++ src/components/containers/MovieListView.js
@@ -3,35 +3,51 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import { loadMovies } from '../../redux/actions/loadMoviesActions';
+import { setOrderAction } from '../../redux/actions/setOrderAction';
 import { rootSaga } from '../../redux/sagas';
 import orderedMovieListSelector from '../../redux/selectors/movieListSelector';
 
 import MovieList from '../presentational/MovieList';
 import SectionHeading from '../presentational/SectionHeading';
 import ViewHeader from '../presentational/ViewHeader';
+import Switch from '../presentational/Switch';
 
 const mapStateToProps = state => ({
 	movies: orderedMovieListSelector(state),
 	loading: state.movieList.loading,
 	error: state.movieList.error,
+	orderBy: state.orderBy,
 });
 const mapDispatchToProps = dispatch => ({
 	getMovies: () => dispatch(loadMovies()),
+	setOrder: order => dispatch(setOrderAction(order)),
 	rootSaga,
 });
 
 const MovieListContainer = props => {
-	const { getMovies, movies, loading, error } = props;
+	const { getMovies, setOrder, movies, loading, error, orderBy } = props;
 	useEffect(() => {
 		const getMoviesOnLoad = () => getMovies();
 		getMoviesOnLoad();
 	}, [getMovies]);
+
+	const isOrderedByDate = orderBy === 'releaseDate';
+
+	const handleOrder = () => {
+		if (orderBy === 'popularity') {
+			setOrder('releaseDate');
+		} else {
+			setOrder('popularity');
+		}
+	};
+
 	if (loading) return <div>Loading...</div>;
 	if (error) return <div>Oops, Something went wrong...</div>;
 	return (
 		<section>
 			<ViewHeader>
 				<SectionHeading>Movies</SectionHeading>
+				<Switch isDate={isOrderedByDate} handleSwitch={handleOrder} />
 			</ViewHeader>
 			<MovieList movies={movies} />
 		</section>
@@ -40,15 +56,18 @@ const MovieListContainer = props => {
 
 MovieListContainer.propTypes = {
 	getMovies: PropTypes.func.isRequired,
+	setOrder: PropTypes.func.isRequired,
 	movies: PropTypes.arrayOf(PropTypes.object),
 	loading: PropTypes.bool,
 	error: PropTypes.string,
+	orderBy: PropTypes.string,
 };
 
 MovieListContainer.defaultProps = {
 	movies: [],
 	loading: false,
 	error: null,
+	orderBy: 'popularity',
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(MovieListContainer);
--- src/redux/actions/setOrderAction.js
+++ src/redux/actions/setOrderAction.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 import { SET_ORDER_BY } from './actionsTypes';
 
-export const setOrder = order => ({
+export const setOrderAction = order => ({
 	type: SET_ORDER_BY,
 	order,
 });
